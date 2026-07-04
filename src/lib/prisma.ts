9303b41c6d83a1e95c5550bd7504fa1cccce9446
rupesh-velocity <rupesh.velocityconsultancy@gmail.com>
2026-07-04 11:00:37 +0530

    Prioritize pooled connection string to fix DB timeouts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as { prismaClientV3: PrismaClient }
 
 export const prisma = globalForPrisma.prismaClientV3 || (() => {
-  const url = process.env.DATABASE_URL || process.env.PRISMA_DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL;
+  const url = process.env.PRISMA_DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL || process.env.POSTGRES_URL;
   return new PrismaClient({
     datasourceUrl: url,
   })
