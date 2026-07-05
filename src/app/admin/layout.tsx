d57ef5b4efab5d2bb83d5031026820b5ee114fdb
rupesh-velocity <rupesh.velocityconsultancy@gmail.com>
2026-07-05 18:53:56 +0530

    Fix sticky behavior in admin layout for editor toolbar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -9,7 +9,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 p-8 overflow-auto">
+        <main className="flex-1 p-8">
           {children}
         </main>
       </div>
